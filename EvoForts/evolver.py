5a3820a8ae2421ba7be3304ff8168fab6d7d05a2
Flodurs <76902177+Flodurs@users.noreply.github.com>
2021-01-03 12:11:35 +0100

    Add files via upload

---
+++ EvoForts/evolver.py
@@ -0,0 +1,9 @@
+class evolver:
+    def __init__(self):
+            print("Evolver Constructor")
+            
+    def __del__(self): 
+        print("Evolver Destructor") 
+        
+    def update(self):
+        
