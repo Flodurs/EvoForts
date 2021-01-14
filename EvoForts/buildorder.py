ff492ec686cc1b464c739d025b8cfa4f2be28d41
Flodurs <76902177+Flodurs@users.noreply.github.com>
2021-01-15 00:07:24 +0100

    Update buildorder.py

--- EvoForts/buildorder.py
+++ EvoForts/buildorder.py
@@ -1,6 +1,32 @@
 class buildorder:
+
+    mOrder = []
+
     def __init__(self):
         print("Buildorder Constructor")
             
     def __del__(self): 
         print("Buildorder Destructor") 
+        
+    def insertActionCreateNode(self,frame,parentNode,relDest):
+        self.insertActionSorted([frame, "cn" , parentNode, relDest[0], relDest[1]])
+        
+    def insertActionCreateLink(self,frame,nodeA,nodeB):
+        self.insertActionSorted([frame, "cn" ,parentNode, nodeA, nodeB])
+    
+    def insertActionSorted(self, action):
+        index = 0
+        
+        if len(self.mOrder) == 0:           #Check for empty Array
+            self.mOrder.append(action)
+            return 
+            
+        for i,order in enumerate(self.mOrder):
+            index = i   
+            
+            if int(action[0]) < int(self.mOrder[i][0]):
+                index = i
+                break
+             
+        
+        self.mOrder.insert(index,action)
